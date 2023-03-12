7153b29d9e2bbe917c76e63138b1367fb2b607c4
Stefan Sabev <stevenbg91@gmail.com>
2023-03-12 17:59:47 +0100

    Initial commit

---
+++ Lab Strings and Text Processing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab_Strings_and_Text_Processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string reversedWord = string.Empty;
+
+                for (int i = input.Length-1; i >= 0; i--)
+                {
+                    reversedWord += input[i];
+
+                }
+                Console.WriteLine($"{input} = {reversedWord}");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
